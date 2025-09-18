2b281f669195a12a4f03bc2b09172b4f3c7c2d89
hoaian19 <hoaian.190903@gmail.com>
2025-09-18 19:58:15 +0700

    Update: edit_cart

--- test/specs/edit_cart.spec.ts
+++ test/specs/edit_cart.spec.ts
@@ -1,54 +1,59 @@
-import { browser, expect } from '@wdio/globals';
+import { $, browser, expect } from '@wdio/globals';
 import loginPage from '../pageobjects/login.page';
+import cartPage from '../pageobjects/cart.page';
+import productsPage from '../pageobjects/products.page';
 
-describe('Order History Functionality', () => {
+describe('Edit Cart Functionality (via Add to Cart)', () => {
     beforeEach(async () => {
-        await browser.url('/');
-        await browser.execute(() => {
-            localStorage.clear();
-            sessionStorage.clear();
-        });
+        await browser.url('http://localhost:3000/');
+        await browser.execute(() => { localStorage.clear(); sessionStorage.clear(); });
         await browser.deleteCookies();
-    });
 
-    it('TC_E2E_VOH_001 - View past orders successfully', async () => {
         await loginPage.open();
         await loginPage.login('john@email.com', '123456');
-        await browser.url('/profile');
-        await expect(browser).toHaveUrl('/profile');
-        const ordersTable = await $('table');
-        await expect(ordersTable).toBeDisplayed();
+        await browser.pause(2000);
+ });
+
+
+    it('TC_E2E_PA0_006 - Update quantity in Cart', async () => {
+        await productsPage.open('/product/68c654b2f62f2bbc3db4e5a4');
+        await productsPage.addToCart();
+        await cartPage.updateQuantity("iPhone 13 Pro 256GB Memory", 2);
+
+        const qtyInput = await cartPage.getProductQuantity("iPhone 13 Pro 256GB Memory");
+        await expect(await qtyInput.getValue()).toBe("2");
     });
 
-    it('TC_E2E_VOH_002 - View order history with no past orders', async () => {
-        await loginPage.open();
-        await loginPage.login('conrad@gmail.com', '123456');
+    it('TC_E2E_PA0_007 - Remove product from Cart', async () => {
+        await productsPage.open('/product/68c654b2f62f2bbc3db4e5a3');
+        await productsPage.addToCart();
 
-        await browser.url('/profile');
+        const productName = "Airpods Wireless Bluetooth Headphones";
+        await cartPage.removeProductByName(productName);
 
-        const noOrderMsg = await $('//*[contains(text(),"Order not found")]');
-        await expect(noOrderMsg).toBeDisplayed();
+        const product = await cartPage.getProduct(productName);
+        await expect(product).not.toBeExisting();
     });
 
-    it('TC_E2E_VOH_003 - View details of an existing order', async () => {
-        await loginPage.open();
-        await loginPage.login('john@email.com', '123456');
-        await browser.url('/profile');
-        const firstDetailsBtn = await $('table tbody tr:first-child a.btn.btn-sm.btn-light');
-        await firstDetailsBtn.click();
+    it('TC_E2E_PA0_008 - Set quantity exceeding stock', async () => {
+        // Add Playstation 5
+        await productsPage.open('/product/68c654b2f62f2bbc3db4e5a4');
+        await productsPage.addToCart();
 
-        await expect(browser).toHaveUrl('/order/');
-        const orderTitle = await $('h1=Order');
-        await expect(orderTitle).toBeDisplayed();
-    });
+        // Set quantity = 11 (exceed stock)
+        await cartPage.updateQuantity("iPhone 13 Pro 256GB Memory", 11);
 
-    it('TC_E2E_VOH_004 - Unauthorized access to another user’s order details', async () => {
-        await loginPage.open();
-        await loginPage.login('httphoaian@gmail.com', 'thechoice19');
-
-        await browser.url('/order/68c7b6c48824a9dc3e33ae00');
-
-        const errorMsg = await $('//*[contains(text(),"Not authorized")]');
+        const errorMsg = await $('//div[contains(text(),"exceeds available stock")]');
         await expect(errorMsg).toBeDisplayed();
+    });
+
+    it('TC_E2E_PA0_009 - Set quantity to 0 (should keep 1)', async () => {
+        await productsPage.open('/product/68c654b2f62f2bbc3db4e5a5');
+        await productsPage.addToCart();
+
+        await cartPage.updateQuantity("Cannon EOS 80D DSLR Camera", 0);
+
+        const qtyInput = await cartPage.getProductQuantity("Cannon EOS 80D DSLR Camera");
+        await expect(await qtyInput.getValue()).toBe("0");
     });
 });
